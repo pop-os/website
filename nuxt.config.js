a1ae6554c2b9c7b2e061e4307ec84b68ce39096f
Blake Kostner <btkostner@gmail.com>
2020-04-20 13:42:30 -0600

    meta the sh*t out of it (#1)

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,15 +7,37 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Imagine an OS for the software developer, maker and computer science professional who uses their computer as a tool to discover and create. Welcome to Pop!_OS.' }
+      { name: 'description', content: 'Imagine an OS for the software developer, maker and computer science professional who uses their computer as a tool to discover and create. Welcome to Pop!_OS.' },
+
+      { name: 'theme-color', content: '#6ACAD8' },
+      { name: 'msapplication-TileColor', content: '#46bac8' },
+
+      { name: 'application-name', content: 'Pop!_OS by System76' },
+      { name: 'apple-mobile-web-app-title', content: 'Pop!_OS' },
+
+      { name: 'og:type', content: 'website' },
+      { name: 'og:title', content: 'Pop!_OS by System76' },
+      { name: 'og:description', content: 'Imagine an OS for the software developer, maker and computer science professional who uses their computer as a tool to discover and create. Welcome to Pop!_OS.' },
+      { name: 'og:image', content: 'https://pop.system76.com/icon.png' },
+      { name: 'og:url', content: 'https://pop.system76.com' },
+
+      { name: 'twitter:site', content: '@system76' },
+      { name: 'twitter:title', content: 'Pop!_OS by System76' },
+      { name: 'twitter:description', content: 'Imagine an OS for the software developer, maker and computer science professional who uses their computer as a tool to discover and create. Welcome to Pop!_OS.' },
+      { name: 'twitter:image', content: 'https://pop.system76.com/icon.png' }
     ],
 
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'manifest', href: '/manifest.json' },
+
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icon-16.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icon-32.png' },
+      { rel: 'application-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#46bac8' }
     ]
   },
 
   loading: {
-    color: '#fff'
+    color: '#6ACAD8'
   }
 }
