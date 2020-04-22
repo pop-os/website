7e6811a16516ef949e71a2a184cd5cdf25373ce8
Blake Kostner <git@btkostner.io>
2020-04-21 20:08:04 -0600

    optimize the head

--- components/index/head.vue
+++ components/index/head.vue
@@ -1,30 +1,7 @@
 <template>
   <section :class="classes">
-    <template v-if="isLight">
-      <img
-        alt="Planet Texture"
-        class="bg-texture"
-        importance="low"
-        src="~/assets/images/index/head-texture-light.png?size=1000"
-      >
-      <img
-        alt="Planets"
-        class="bg-planets"
-        importance="low"
-        src="~/assets/images/index/head-planet-light.png?size=1600"
-      >
-    </template>
-    <template v-if="isDark">
-      <img
-        alt="Moon Texture"
-        class="bg-moon"
-        importance="low"
-        src="~/assets/images/index/head-moon-dark.svg"
-      >
-    </template>
-
     <div class="content">
-      <div>
+      <div class="copy">
         <sys-subheader-1>
           Welcome to Pop!_OS
         </sys-subheader-1>
@@ -57,10 +34,13 @@
 
 <style scoped>
   section {
+    align-content: center;
+    align-items: center;
     clip-path: ellipse(140% calc(100vw + 6rem) at 50% calc(100% - 6rem - 100vw));
+    display: flex;
+    justify-content: center;
     position: relative;
     width: 100%;
-    overflow: hidden;
   }
 
   section.light-mode {
@@ -68,9 +48,40 @@
     color: #fff;
   }
 
+  section.light-mode::before {
+    background-image: url("~assets/images/index/head-texture-light.png?size=800");
+    background-size: cover;
+    bottom: 0;
+    content: "";
+    height: 100%;
+    left: 0;
+    mix-blend-mode: luminosity;
+    opacity: 0.4;
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 100%;
+    z-index: 1;
+  }
+
   section.dark-mode {
     background: linear-gradient(#fff 0%, #fcfbf7 100%);
     color: #574F4A;
+  }
+
+  section.dark-mode::before {
+    background-image: url("~assets/images/index/moon-dark.svg");
+    background-position: bottom center;
+    background-size: cover;
+    bottom: 0;
+    content: "";
+    height: 100%;
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 100%;
+    z-index: 1;
   }
 
   section.dark-mode::after {
@@ -85,50 +96,19 @@
     z-index: 2;
   }
 
-  .bg-planets {
-    position: absolute;
-    top: 1rem;
-    left: calc(50% - 900px);
-    width: 1600px;
-    z-index: 2;
-    opacity: 0.2;
-  }
-
-  .bg-texture {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-    mix-blend-mode: luminosity;
-    opacity: 0.5;
-  }
-
-  .bg-moon {
-    position: absolute;
-    left: 0;
-    right: 0;
-    object-fit: cover;
-    object-position: bottom center;
-    bottom: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-  }
-
   .content {
-    display: grid;
-    grid-gap: 1rem;
-    grid-template-columns: 1fr;
-    margin: 0 auto 3rem;
+    align-content: center;
+    align-items: flex-start;
+    display: flex;
+    justify-content: center;
     max-width: 1280px;
-    padding: 2rem 1rem 0;
-    position: relative;
+    padding: 4rem 1rem 6rem;
+    width: 100%;
     z-index: 10;
+  }
+
+  .copy {
+    max-width: 80ch;
   }
 
   h1 {
@@ -138,34 +118,47 @@
   .logo {
     display: none;
     filter: drop-shadow(4px 4px 6px rgba(0, 0, 0, 0.35));
-    max-width: 100%;
+    flex: 1 1 auto;
+    margin-left: 1rem;
+    max-width: 80ch;
   }
 
-  @media (width >= 40rem) {
-    .content {
-      margin-bottom: 5rem;
-    }
-  }
-
-  @media (width >= 70rem) {
-    .content {
-      grid-template-columns: 1fr 1fr;
-      margin-bottom: 7rem;
+  @media (width >= 800px) {
+    section.light-mode::before {
+      background-image: url("~assets/images/index/head-texture-light.png?size=2000");
     }
 
-    .logo {
-      display: block;
+    section.light-mode::after {
+      background-image: url("~assets/images/index/head-planet-light.png?size=1600");
+      content: "";
+      height: 477.6px;
+      left: calc(50% - 900px);
+      opacity: 0.2;
+      position: absolute;
+      top: 1rem;
+      width: 1600px;
+      z-index: 2;
     }
   }
 
   @media (width >= 1280px) {
     section {
-      clip-path: ellipse(calc(53vw + 242px) 140% at 45vw -40%);
+      clip-path: ellipse(calc(56vw + 345px) 140% at 41vw -40%);
     }
 
     .content {
-      padding-top: 4vw;
-      padding-bottom: calc(8rem - 4vw);
+      max-width: 1280px;
+      padding: 6rem 1rem 8rem;
+      justify-content: space-between;
+    }
+
+    .copy {
+      max-width: 68ch;
+    }
+
+    .logo {
+      display: block;
+      max-width: 56ch;
     }
   }
 </style>
