3f30d8ebfe4a779ae90cd7070b686123f3c695b0
Blake Kostner <btkostner@gmail.com>
2020-11-06 14:16:07 -0700

    update pop sponsors config (#26)

--- config/production.js
+++ config/production.js
@@ -1,5 +1,5 @@
 module.exports = {
   API_URL: 'https://api-v2.system76.com',
-  SPONSOR_URL: 'https://sponsors.system76.com',
+  SPONSOR_URL: 'https://sponsors.pop-os.org',
   STRIPE_KEY: 'pk_live_LvaggT7SlV0xsTG4gNBT2JPq'
 }
--- config/test.js
+++ config/test.js
@@ -1,5 +1,5 @@
 module.exports = {
   API_URL: 'https://api-v2.genesis76.com',
-  SPONSOR_URL: 'https://sponsors.genesis76.com',
+  SPONSOR_URL: 'https://sponsors.so-dod.org',
   STRIPE_KEY: 'pk_test_xJKcbnti9vfXEfV4l1waNwfT'
 }
