de39507645f902eb9d54e1845157de17af4f2d5a
Mike Garegnani <systems.support@gmail.com>
2022-03-25 16:49:20 -0700

    Add privacy statement to newsletter modal

---
+++ components/privacy-paragraph.vue
@@ -0,0 +1,25 @@
+<template>
+  <p class="legal">
+    <em>Your email address will be used in accordance with our <a
+      href="https://system76.com/privacy"
+      target="_blank"
+    >privacy policy</a>.</em>
+  </p>
+</template>
+
+<style scoped>
+  p.legal {
+    font-size: 0.9em;
+    text-align: center;
+    margin: 1em 0;
+  }
+  a {
+    color: var(--color-orange);
+  }
+</style>
+
+<script>
+  export default {
+    name: 'PrivacyParagraph'
+  }
+</script>
